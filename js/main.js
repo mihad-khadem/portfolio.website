d210a63458af36c5c14790ec7eedc4e464e833de
mihad-khadem <mihadkhadem@gmail.com>
2023-08-23 22:33:18 +0600

    Copy => Mail issue Fixed

--- js/main.js
+++ js/main.js
@@ -47,7 +47,7 @@ navLinks.forEach((n) => n.addEventListener("click", () => { navMenu.classList.re
 /*===== COPY Email =====*/
 const copy = document.getElementById("copy");
 copy.addEventListener("click", () => {
-  navigator.clipboard.writeText("kasulaarunteja@gmail.com");
+  navigator.clipboard.writeText("mihadkhadem@gmail.com");
   copy.innerHTML = "copied";
   setTimeout(() => {
     copy.innerHTML = null;
